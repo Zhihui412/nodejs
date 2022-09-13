f5245fff8f3dc35f55895ef906be0b71e78f0045
huangzhihui <leononon412@gmail.com>
2022-09-13 15:54:09 +0800

    add blog route finish

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,6 +2,34 @@ const querystringify = require('querystringify')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// 处理 POST Data
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+    return promise
+}
+
 const serverHandle = (req, res) => {
     // 设置返回 格式 JSON
     res.setHeader('Content-type', 'application/json')
@@ -11,30 +39,38 @@ const serverHandle = (req, res) => {
     req.path = url.split('?')[0]
 
     // 解析 query
-    req.query = querystringify.parse(url.split('?')[0])
+    req.query = querystringify.parse(url.split('?')[1])
 
-    // 处理 blog 路由
-    const blogData = handleBlogRouter(req, res)
-    if (blogData) {
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return 
-    }
+    //处理路由之前对post data进行解析
+    getPostData(req).then(postData => {
+        // 将post放入req中方便后面路由使用
+        req.body = postData
 
-    // 处理 user 路由
-    const userData = handleUserRouter(req, res)
-    if (userData) {
-        res.end(
-            JSON.stringify(userData)        
-        )
-        return
-    }
+        // 处理 blog 路由
+        const blogData = handleBlogRouter(req, res)
+        if (blogData) {
+            res.end(
+                JSON.stringify(blogData)
+            )
+            return
+        }
 
-    // 未命中路由， 返回404
-    res.writeHead(404, {"Content-type": "text/plain"})
-    res.write("404 Not Found\n")
-    res.end( )
+        // 处理 user 路由
+        const userData = handleUserRouter(req, res)
+        if (userData) {
+            res.end(
+                JSON.stringify(userData)
+            )
+            return
+        }
+
+        // 未命中路由， 返回404
+        res.writeHead(404, { "Content-type": "text/plain" })
+        res.write("404 Not Found\n")
+        res.end()
+    })
+
+
 
 }
 
--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -18,6 +18,45 @@ const getList = (author, keyword) => {
     ]
 }
 
+const getDetail = (id) => {
+    return {
+        id: 1,
+        title: '标题111',
+        content: '内容111',
+        createTime: '1546610491112',
+        author: 'zhangsan'
+    }
+}
+
+const newBlog = (blogData = {}) => {
+    // blogData 是一个博客对象， 包含 title content 属性
+    console.log('newBlog blogData...', blogData);
+    
+    return {
+        id: 3 // 表示i虚拟键博客，插入到数据表里面的 id
+    }
+}
+
+const updateBlog = (id, blogData = {}) => {
+    // id 就是要更新博客的 id
+    // blogData 是一个博客对象， 包含 title content 属性
+    console.log('update blog', id, blogData);
+
+    return true
+    
+}
+
+const delBlog = (id) => {
+    // id 要删除的博客的id 
+
+    return true
+}
+
+
 module.exports = {
-    getList
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
 }
