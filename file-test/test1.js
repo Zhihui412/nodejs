0ae6485b410ce9b3c701a9665412fcae6591aeaf
huangzhihui <leononon412@gmail.com>
2022-09-21 01:06:23 +0800

    add file-test

---
+++ file-test/test1.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+const fileName = path.resolve(__dirname, 'data.txt')
+
+
+// 读取文件
+// fs.readFile(fileName, (err, data) => {
+//     if (err){
+//         console.error('读取错误');
+//         return
+//     }
+//     console.log(data.toString());
+    
+// })
+
+// 写入内容
+// const content = 'this is new txt'
+// const opt = {
+//     flag: 'a' // a表示在原文本添加内容， w表示重写覆盖原文本
+// }
+
+// fs.writeFile(fileName, content, opt, (err) => {
+//     if (err){ 
+//         console.error(err)  
+//     }
+// })
+
+// 判断文件是否存在
+// fs.exists(fileName, (exist) => {
+//     console.log('exist', exist);
+// })
